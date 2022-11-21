ef91255a315a174f4bbb0c0b071119f48de61821
zhonghuafy <zhonghuafy@hotmail.com>
2022-11-21 16:56:29 +0800

    sentinel flow control

--- sp-structure-application/src/main/java/org/fe/ek/test/proj/stucture/util/BlockHandler.java
+++ sp-structure-application/src/main/java/org/fe/ek/test/proj/stucture/util/BlockHandler.java
@@ -1,6 +1,8 @@
 package org.fe.ek.test.proj.stucture.util;
 
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson2.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.fe.ek.test.common.exp.CmErrCode;
 import org.fe.ek.test.common.po.ResultPO;
 
@@ -11,6 +13,7 @@ import org.fe.ek.test.common.po.ResultPO;
  * @create: 2022-11-21
  * @version: v1.0.0 创建文件, Wang Zhenhua, 2022-11-21
  **/
+@Slf4j
 public class BlockHandler {
 
     /**
@@ -19,6 +22,7 @@ public class BlockHandler {
      * @return
      */
     public static ResultPO handleBlock(BlockException bEx) {
+        log.info("接口限流 ruleLimitApp: {} rule: {}", bEx.getRuleLimitApp(), JSON.toJSONString(bEx.getRule()));
         return new ResultPO(CmErrCode.E100006);
     }
 }
